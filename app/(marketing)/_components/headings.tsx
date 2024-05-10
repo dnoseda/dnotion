dbdf56c2914e10bf4f1c4802b73a97fa2753235a
dnoseda <damian.noseda@mercadolibre.com>
2024-05-10 15:48:19 -0300

    components done

---
+++ app/(marketing)/_components/headings.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export const Heading = () => {
+    return (
+        <div className="mx-w-3xl space-y-4">
+            <h1>
+                Your Ideas, Documents & plans, organized with .commands. Write a good one! <span className="underline">D Notion</span>
+            </h1>
+        </div>
+    )
+}
